0516ca1da7b3cf237bc83fba382f1f25d38838b7
Indhumathi Kumar <indhu0074@gmail.com>
2025-03-07 23:21:28 +0530

    Service-Angular

---
+++ src/app/user-list/user-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit} from '@angular/core';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-user-list',
+  imports: [],
+  templateUrl: './user-list.component.html',
+  styleUrl: './user-list.component.css'
+})
+export class UserListComponent {
+
+  users = [];
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    });
+  }
+}
